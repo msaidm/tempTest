295e414108d07b9240a482110d1215248bb12885
msaidHectar <m.said@hectar.io>
2023-06-25 19:45:15 +0300

    ds

--- api.test.js
+++ api.test.js
@@ -8,7 +8,7 @@ describe('API Routes', () => {
   it('should create a new user', async () => {
     const response = await request('http://localhost:3000')
       .post('/api/v1/users')
-      .send({ name: 'marr', email: 'marhy@gmail.com', password: '123' });
+      .send({ name: 'marwr', email: 'marhyds@gmail.com', password: '123' });
 
     expect(response.status).to.equal(200);
     expect(response.body).to.have.property('message', 'User registered with success');
